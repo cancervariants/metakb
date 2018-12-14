51d3bfcded0c80fc152b6fe49e0ff5f85dc3871f
Brian Walsh <brian@bwalsh.com>
2018-12-14 05:53:18 -0800

    adds module setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='metakb',
+      version='0.0.0',
+      description='Central repository for the VICC metakb web application',
+      url='https://github.com/cancervariants/metakb',
+      author='VICC',
+      author_email='vicc_leaders@genomicsandhealth.org',
+      license='MIT',
+      packages=['metakb'],
+      zip_safe=False)
