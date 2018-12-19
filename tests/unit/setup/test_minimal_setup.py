7a2a5c2c369c47cae3d2765687f10a00d558fc71
Alex H. Wagner, PhD <awagner24@wustl.edu>
2018-12-19 09:59:42 -0800

    Update tests/unit/setup/test_minimal_setup.py
    Co-Authored-By: bwalsh <brian@bwalsh.com>

--- tests/unit/setup/test_minimal_setup.py
+++ tests/unit/setup/test_minimal_setup.py
@@ -6,7 +6,7 @@ import sys
 def test_python_version():
     """Should use python 3.7.1."""
     assert sys.version_info.major == 3, "Must use Python 3"
-    assert sys.version_info.minor >= 7, "Must use Python 3.7"
+    assert sys.version_info.minor >= 7, "Must use at least Python 3.7"
 
 
 def test_import_main_package():
