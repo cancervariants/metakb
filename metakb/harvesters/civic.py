57074f858ffd97c1130f7feafe1cb6744c3c4ba0
korikuzma <korikuzma@gmail.com>
2020-11-24 13:04:25 -0500

    WIP: Use civicpy to implement civic harvester for genes

--- metakb/harvesters/civic.py
+++ metakb/harvesters/civic.py
@@ -1,5 +1,5 @@
 """A module for the CIViC harvester."""
-import requests
+from civicpy import civic
 import json
 from pathlib import Path
 from timeit import default_timer as timer
@@ -10,61 +10,65 @@ class CIViC:
 
     def __init__(self):
         self.project_root = Path(__file__).resolve().parents[2]
-        self.genes_url = 'https://civicdb.org/api/genes'
 
     def harvest(self):
         """Retrieve and store records from CIViC."""
-        self.harvest_gene()
+        genes_list = self.harvest_gene()
+
 
     def harvest_gene(self):
         """Harvest gene information."""
-        start = timer()
-        self._get_gene_information()
-        end = timer()
-        print(end-start)
-        return True
+        genes = civic.get_all_genes()
+        genes_list = list()
+        for gene in genes:
+            g = {
+                'id': gene.id,
+                'name': gene.name,
+                'entrez_id': gene.entrez_id,
+                'description': gene.description,
+                'variants': [
+                    {
+                        'name': variant.name,
+                        'id': variant.id,
+                        'evidence_items_temp': [
+                            e.status for e in variant.evidence_items
+                        ]
+                    }
+                    for variant in gene.variants
+                ],
+                'aliases': gene.aliases,
+                # TODO: Add sources
+                'type': gene.type
+            }
 
-    def _get_gene_information(self):
-        r = requests.get(self.genes_url)
-        r_json = r.json()
-        next_link = r_json['_meta']['links']['next']
-        all_links = [self.genes_url]
-        records_json = list()
+            # Update evidence_items
+            for v in g['variants']:
+                evidence_items = {
+                    'accepted_count': 0,
+                    'rejected_count': 0,
+                    'submitted_count': 0
+                }
+                for e in v['evidence_items_temp']:
+                    if e == 'accepted':
+                        evidence_items['accepted_count'] = \
+                            evidence_items['accepted_count'] + 1
+                    elif e == 'submitted':
+                        evidence_items['submitted_count'] = \
+                            evidence_items['submitted_count'] + 1
+                    elif e == 'rejected':
+                        evidence_items['rejected_count'] = \
+                            evidence_items['rejected_count'] + 1
+                del v['evidence_items_temp']
+                v['evidence_items'] = evidence_items
+            genes_list.append(g)
+        return genes_list
 
-        while next_link:
-            all_links.append(next_link)
-            r = requests.get(next_link)
-            r_json = r.json()
-            next_link = r_json['_meta']['links']['next']
-
-        for link in all_links:
-            r = requests.get(link)
-            r_json = r.json()
-            records = r_json['records']
-            for record in records:
-                gene_url = f"{self.genes_url}/{record['name']}" \
-                           f"?identifier_type=entrez_symbol"
-                r = requests.get(gene_url)
-                gene = r.json()
-
-                records_json.append({
-                    'id': gene['id'],
-                    'name': gene['name'],
-                    'entrez_id': gene['entrez_id'],
-                    'description': gene['description'],
-                    'variants': gene['variants'],
-                    'aliases': gene['aliases'],
-                    'type': gene['type'],
-                    'lifecycle_actions': gene['lifecycle_actions'],
-                    'sources': gene['sources'],
-                    'provisional_values': gene['provisional_values'],
-                    'errors': gene['errors']
-                })
-
-        with open(f"{self.project_root}/data/civic/gene.json", 'w+') as f:
-            f.write(json.dumps(records_json))
-            f.close()
+    def create_json(self, genes_list):
+        """Create comprehensive json file containing gene,
+        evidence, and variant data.
+        """
+        pass
 
 
-civic = CIViC()
-civic.harvest()
+c = CIViC()
+c.harvest()
