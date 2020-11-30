97dcc13e4f32e978ba15990b94326bb4322e92fc
korikuzma <korikuzma@gmail.com>
2020-11-30 09:35:18 -0500

    Import project root

--- metakb/__init__.py
+++ metakb/__init__.py
@@ -1,1 +1,7 @@
 """The MetaKB package."""
+from pathlib import Path
+import logging
+
+PROJECT_ROOT = Path(__file__).resolve().parents[1]
+logger = logging.getLogger('metakb')
+logger.setLevel(logging.DEBUG)
