8c280ef5f0ec89634d80a270c856d3c328808f72
korikuzma <korikuzma@gmail.com>
2020-12-01 09:03:38 -0500

    Split up civic harvester tests

---
+++ tests/unit/harvesters/test_civic_evidence.py
@@ -0,0 +1,132 @@
+"""Test CIViC source"""
+import pytest
+from metakb.harvesters.civic import CIViC
+
+
+@pytest.fixture(scope='module')
+def evidence():
+    """Create a list of evidence."""
+    c = CIViC()
+    return c.harvest_evidence()
+
+
+@pytest.fixture(scope='module')
+def lnscc():
+    """Create a fixture for EID3017 evidence."""
+    return {
+        "id": 3017,
+        "name": "EID3017",
+        "description": "Patients with BRAF V600E-mutant NSCLC (n=57) were "
+                       "enrolled into a phase 2, multicentre, non-randomised, "
+                       "open-label study, administering dabrafenib plus "
+                       "trametinib. The overall response rate was 36/57 "
+                       "(63.2%, [95% CI 49.3-75.6]) and the median "
+                       "progression-free survival was 9.7 months "
+                       "(95% CI 6.9-19.6). At data cutoff "
+                       "(11.6 months of follow-up), 18/36  (50%) confirmed "
+                       "responses were ongoing and 23/57 (40%) of patients "
+                       "had died.",
+        "disease": {
+            "id": 8,
+            "name": "Lung Non-small Cell Carcinoma",
+            "display_name": "Lung Non-small Cell Carcinoma",
+            "doid": "3908",
+            "url": "http://www.disease-ontology.org/?id=DOID:3908"
+        },
+        "drugs": [
+            {
+                "id": 19,
+                "name": "Trametinib",
+                "ncit_id": "C77908",
+                "aliases": [
+                    "N-(3-{3-cyclopropyl-5-[(2-fluoro-4-iodophenyl)amino]-6"
+                    ",8-dimethyl-2,4,7-trioxo-3,4,6,7-tetrahydropyrido[4,3-d]"
+                    "pyrimidin-1(2H)-yl}phenyl)acetamide",
+                    "Mekinist", "MEK Inhibitor GSK1120212", "JTP-74057",
+                    "GSK1120212"]
+            }, {
+                "id": 22,
+                "name": "Dabrafenib",
+                "ncit_id": "C82386",
+                "aliases": ["GSK2118436", "GSK-2118436A", "GSK-2118436",
+                            "BRAF Inhibitor GSK2118436",
+                            "Benzenesulfonamide, N-(3-(5-(2-amino-4-pyrimidin"
+                            "yl)-2-(1,1-dimethylethyl)-4-thiazolyl)-2-fluor"
+                            "ophenyl)-2,6-difluoro-"]
+            }
+        ],
+        "rating": 4,
+        "evidence_level": "A",
+        "evidence_type": "Predictive",
+        "clinical_significance": "Sensitivity/Response",
+        "evidence_direction": "Supports",
+        "variant_origin": "Somatic",
+        "drug_interaction_type": "Combination",
+        "status": "accepted",
+        # "open_change_count": 0,
+        "type": "evidence",
+        "source": {
+            "id": 1296,
+            "name": "Dabrafenib plus trametinib in patients with previously "
+                    "treated BRAF(V600E)-mutant metastatic non-small cell "
+                    "lung cancer: an open-label, multicentre phase 2 trial.",
+            "citation": "Planchard et al., 2016, Lancet Oncol.",
+            "citation_id": "27283860",
+            "source_type": "PubMed",
+            "asco_abstract_id": None,
+            "source_url": "http://www.ncbi.nlm.nih.gov/pubmed/27283860",
+            "open_access": True,
+            "pmc_id": "PMC4993103",
+            "publication_date": {
+                "year": 2016,
+                "month": 7
+            },
+            "journal": "Lancet Oncol.",
+            "full_journal_title": "The Lancet. Oncology",
+            "status": "partially curated",
+            "is_review": False,
+            "clinical_trials": [{
+                "nct_id": "NCT01336634",
+                "name": "Study of Selective BRAF Kinase Inhibitor Dabrafenib "
+                        "Monotherapy Twice Daily and in Combination With "
+                        "Dabrafenib Twice Daily and Trametinib Once Daily in "
+                        "Combination Therapy in Subjects With BRAF V600E "
+                        "Mutation Positive Metastatic (Stage IV) Non-small "
+                        "Cell Lung Cancer.",
+                "description": "Dabrafenib is a potent and selective inhibitor"
+                               " of BRAF kinase activity. This is a Phase II,"
+                               " non-randomized, open-label study to assess "
+                               "the efficacy, safety, and tolerability of "
+                               "dabrafenib administered as a single agent and "
+                               "in combination with trametinib in stage IV "
+                               "disease to subjects with BRAF mutant advanced"
+                               " non-small cell lung cancer. Subjects will "
+                               "receive dabrafenib 150 mg twice daily (BID) "
+                               "in monotherapy treatment and dabrafenib 150 "
+                               "mg bid and trametinib 2 mg once daily in "
+                               "combination therapy and continue on treatment"
+                               " until disease progression, death, or "
+                               "unacceptable adverse event.",
+                "clinical_trial_url":
+                    "https://clinicaltrials.gov/show/NCT01336634"
+            }]
+        },
+        "variant_id": 12,
+        "phenotypes": [],
+        "assertions": [],
+        # "errors": {},
+        # "fields_with_pending_changes": {},
+        "gene_id": 5
+    }
+
+
+def test_evidence_json(lnscc, evidence):
+    """Test civic harvester works correctly for evidence."""
+    for ev in evidence:
+        if ev['id'] == 3017:
+            actual = ev
+            break
+    assert actual.keys() == lnscc.keys()
+    keys = lnscc.keys()
+    for key in keys:
+        assert actual[key] == lnscc[key]
---
+++ tests/unit/harvesters/test_civic_genes.py
@@ -0,0 +1,565 @@
+"""Test CIViC source"""
+import pytest
+from metakb.harvesters.civic import CIViC
+
+
+@pytest.fixture(scope='module')
+def genes():
+    """Create a list of genes."""
+    c = CIViC()
+    return c.harvest_genes()
+
+
+@pytest.fixture(scope='module')
+def alk():
+    """Create a fixture for ALK gene."""
+    return {
+        'id': 1,
+        'name': "ALK",
+        'entrez_id': 238,
+        'description': "ALK amplifications, fusions and mutations have been "
+                       "shown to be driving events in non-small cell lung "
+                       "cancer. While crizontinib has demonstrated efficacy "
+                       "in treating the amplification, mutations in ALK have "
+                       "been shown to confer resistance to current tyrosine"
+                       " kinase inhibitors. Second-generation TKI's have "
+                       "seen varied success in treating these resistant "
+                       "cases, and the HSP90 inhibitor 17-AAG has been "
+                       "shown to be cytostatic in ALK-altered cell lines.",
+        'variants': [
+            {
+                'name': "CAD-ALK",
+                'id': 2769,
+                'evidence_items': {
+                    'accepted_count': 1,
+                    'rejected_count': 0,
+                    'submitted_count': 0
+                }
+            },
+            {
+                'name': "EML4-ALK (E6;A19) G1269A and AMPLIFICATION",
+                'id': 3204,
+                'evidence_items': {
+                    'accepted_count': 0,
+                    'rejected_count': 0,
+                    'submitted_count': 1
+                }
+            },
+            {
+                'name': "EML4-ALK G1202del",
+                'id': 2813,
+                'evidence_items': {
+                    'accepted_count': 0,
+                    'rejected_count': 0,
+                    'submitted_count': 1
+                }
+            },
+            {
+                'name': "EML4-ALK L1152R",
+                'id': 307,
+                'evidence_items': {
+                    'accepted_count': 1,
+                    'rejected_count': 0,
+                    'submitted_count': 0
+                }
+            },
+            {
+                'name': "EML4-ALK L1198F",
+                'id': 2816,
+                'evidence_items': {
+                    'accepted_count': 0,
+                    'rejected_count': 0,
+                    'submitted_count': 1
+                }
+            },
+            {
+                'name': "L1198F",
+                'id': 1275,
+                'evidence_items': {
+                    'accepted_count': 0,
+                    'rejected_count': 1,
+                    'submitted_count': 1
+                }
+            },
+            {
+                'name': "HIP1-ALK I1171N",
+                'id': 588,
+                'evidence_items': {
+                    'accepted_count': 1,
+                    'rejected_count': 0,
+                    'submitted_count': 2
+                }
+            },
+            {
+                'name': "L1196Q",
+                'id': 1553,
+                'evidence_items': {
+                    'accepted_count': 0,
+                    'rejected_count': 0,
+                    'submitted_count': 1
+                }
+            },
+            {
+                'name': "RANBP2-ALK",
+                'id': 514,
+                'evidence_items': {
+                    'accepted_count': 1,
+                    'rejected_count': 0,
+                    'submitted_count': 0
+                }
+            },
+            {
+                'name': "EML4-ALK L1196M and L1198F",
+                'id': 2810,
+                'evidence_items': {
+                    'accepted_count': 0,
+                    'rejected_count': 0,
+                    'submitted_count': 1
+                }
+            },
+            {
+                'name': "F1174C",
+                'id': 1492,
+                'evidence_items': {
+                    'accepted_count': 0,
+                    'rejected_count': 0,
+                    'submitted_count': 1
+                }
+            },
+            {
+                'name': "R214H",
+                'id': 1683,
+                'evidence_items': {
+                    'accepted_count': 0,
+                    'rejected_count': 0,
+                    'submitted_count': 1
+                }
+            },
+            {
+                'name': "EML4-ALK C1156Y",
+                'id': 6,
+                'evidence_items': {
+                    'accepted_count': 4,
+                    'rejected_count': 2,
+                    'submitted_count': 7
+                }
+            },
+            {
+                'name': "EML4-ALK G1202R and L1196M",
+                'id': 2809,
+                'evidence_items': {
+                    'accepted_count': 0,
+                    'rejected_count': 0,
+                    'submitted_count': 2
+                }
+            },
+            {
+                'name': "T1151M",
+                'id': 1493,
+                'evidence_items': {
+                    'accepted_count': 0,
+                    'rejected_count': 0,
+                    'submitted_count': 1
+                }
+            },
+            {
+                'name': "EML4-ALK  V1180L",
+                'id': 528,
+                'evidence_items': {
+                    'accepted_count': 5,
+                    'rejected_count': 0,
+                    'submitted_count': 0
+                }
+            },
+            {
+                'name': "R1192P",
+                'id': 1661,
+                'evidence_items': {
+                    'accepted_count': 0,
+                    'rejected_count': 1,
+                    'submitted_count': 2
+                }
+            },
+            {
+                'name': "DEL4-11",
+                'id': 550,
+                'evidence_items': {
+                    'accepted_count': 1,
+                    'rejected_count': 0,
+                    'submitted_count': 0
+                }
+            },
+            {
+                'name': "EML6-ALK E1;A20 and FBXO11-ALK E1;A20",
+                'id': 2750,
+                'evidence_items': {
+                    'accepted_count': 0,
+                    'rejected_count': 0,
+                    'submitted_count': 1
+                }
+            },
+            {
+                'name': "MUTATION",
+                'id': 512,
+                'evidence_items': {
+                    'accepted_count': 1,
+                    'rejected_count': 0,
+                    'submitted_count': 0
+                }
+            },
+            {
+                'name': "EML4-ALK",
+                'id': 5,
+                'evidence_items': {
+                    'accepted_count': 7,
+                    'rejected_count': 0,
+                    'submitted_count': 1
+                }
+            },
+            {
+                'name': "EML4-ALK S1206Y",
+                'id': 172,
+                'evidence_items': {
+                    'accepted_count': 2,
+                    'rejected_count': 0,
+                    'submitted_count': 2
+                }
+            },
+            {
+                'name': "EML4-ALK and AMPLIFICATION",
+                'id': 170,
+                'evidence_items': {
+                    'accepted_count': 3,
+                    'rejected_count': 0,
+                    'submitted_count': 0
+                }
+            },
+            {
+                'name': "EXPRESSION",
+                'id': 2914,
+                'evidence_items': {
+                    'accepted_count': 0,
+                    'rejected_count': 0,
+                    'submitted_count': 1
+                }
+            },
+            {
+                'name': "CLTC-ALK",
+                'id': 520,
+                'evidence_items': {
+                    'accepted_count': 3,
+                    'rejected_count': 0,
+                    'submitted_count': 0
+                }
+            },
+            {
+                'name': "G1128A",
+                'id': 2798,
+                'evidence_items': {
+                    'accepted_count': 0,
+                    'rejected_count': 1,
+                    'submitted_count': 1
+                }
+            },
+            {
+                'name': "EML4-ALK E2;A20",
+                'id': 501,
+                'evidence_items': {
+                    'accepted_count': 2,
+                    'rejected_count': 0,
+                    'submitted_count': 0
+                }
+            },
+            {
+                'name': "EML4-ALK E6;A20",
+                'id': 503,
+                'evidence_items': {
+                    'accepted_count': 7,
+                    'rejected_count': 0,
+                    'submitted_count': 0
+                }
+            },
+            {
+                'name': "R1275Q",
+                'id': 9,
+                'evidence_items': {
+                    'accepted_count': 6,
+                    'rejected_count': 0,
+                    'submitted_count': 3
+                }
+            },
+            {
+                'name': "F1245V",
+                'id': 1295,
+                'evidence_items': {
+                    'accepted_count': 1,
+                    'rejected_count': 0,
+                    'submitted_count': 1
+                }
+            },
+            {
+                'name': "ALTERNATIVE TRANSCRIPT (ATI)",
+                'id': 839,
+                'evidence_items': {
+                    'accepted_count': 2,
+                    'rejected_count': 0,
+                    'submitted_count': 2
+                }
+            },
+            {
+                'name': "EML4-ALK I1171S",
+                'id': 589,
+                'evidence_items': {
+                    'accepted_count': 1,
+                    'rejected_count': 0,
+                    'submitted_count': 1
+                }
+            },
+            {
+                'name': "F1174V",
+                'id': 1505,
+                'evidence_items': {
+                    'accepted_count': 0,
+                    'rejected_count': 0,
+                    'submitted_count': 1
+                }
+            },
+            {
+                'name': "F1174L",
+                'id': 8,
+                'evidence_items': {
+                    'accepted_count': 9,
+                    'rejected_count': 0,
+                    'submitted_count': 3
+                }
+            },
+            {
+                'name': "STRN-ALK",
+                'id': 2218,
+                'evidence_items': {
+                    'accepted_count': 1,
+                    'rejected_count': 0,
+                    'submitted_count': 0
+                }
+            },
+            {
+                'name': "ALK FUSION G1269A",
+                'id': 552,
+                'evidence_items': {
+                    'accepted_count': 4,
+                    'rejected_count': 0,
+                    'submitted_count': 0
+                }
+            },
+            {
+                'name': "ALK FUSION G1202R",
+                'id': 171,
+                'evidence_items': {
+                    'accepted_count': 6,
+                    'rejected_count': 0,
+                    'submitted_count': 3
+                }
+            },
+            {
+                'name': "ALK FUSION I1171",
+                'id': 527,
+                'evidence_items': {
+                    'accepted_count': 7,
+                    'rejected_count': 0,
+                    'submitted_count': 3
+                }
+            },
+            {
+                'name': "EML4-ALK G1269A",
+                'id': 308,
+                'evidence_items': {
+                    'accepted_count': 2,
+                    'rejected_count': 0,
+                    'submitted_count': 6
+                }
+            },
+            {
+                'name': "EML4-ALK L1196M",
+                'id': 7,
+                'evidence_items': {
+                    'accepted_count': 4,
+                    'rejected_count': 0,
+                    'submitted_count': 11
+                }
+            },
+            {
+                'name': "F1245C",
+                'id': 549,
+                'evidence_items': {
+                    'accepted_count': 2,
+                    'rejected_count': 0,
+                    'submitted_count': 0
+                }
+            },
+            {
+                'name': "L1198P",
+                'id': 1556,
+                'evidence_items': {
+                    'accepted_count': 0,
+                    'rejected_count': 0,
+                    'submitted_count': 1
+                }
+            },
+            {
+                'name': "L1152P",
+                'id': 1554,
+                'evidence_items': {
+                    'accepted_count': 0,
+                    'rejected_count': 0,
+                    'submitted_count': 1
+                }
+            },
+            {
+                'name': "EML4-ALK G1202R and L1198F",
+                'id': 2811,
+                'evidence_items': {
+                    'accepted_count': 0,
+                    'rejected_count': 0,
+                    'submitted_count': 1
+                }
+            },
+            {
+                'name': "EML4-ALK E20;A20",
+                'id': 500,
+                'evidence_items': {
+                    'accepted_count': 4,
+                    'rejected_count': 0,
+                    'submitted_count': 0
+                }
+            },
+            {
+                'name': "ALK FUSIONS",
+                'id': 499,
+                'evidence_items': {
+                    'accepted_count': 31,
+                    'rejected_count': 2,
+                    'submitted_count': 10
+                }
+            },
+            {
+                'name': "ALK FUSION F1245C",
+                'id': 551,
+                'evidence_items': {
+                    'accepted_count': 3,
+                    'rejected_count': 0,
+                    'submitted_count': 0
+                }
+            },
+            {
+                'name': "ALK FUSION L1196M",
+                'id': 2819,
+                'evidence_items': {
+                    'accepted_count': 0,
+                    'rejected_count': 0,
+                    'submitted_count': 1
+                }
+            },
+            {
+                'name': "EML4-ALK C1156Y-L1198F",
+                'id': 352,
+                'evidence_items': {
+                    'accepted_count': 2,
+                    'rejected_count': 0,
+                    'submitted_count': 1
+                }
+            },
+            {
+                'name': "NPM-ALK",
+                'id': 513,
+                'evidence_items': {
+                    'accepted_count': 3,
+                    'rejected_count': 1,
+                    'submitted_count': 0
+                }
+            },
+            {
+                'name': "OVEREXPRESSION",
+                'id': 2635,
+                'evidence_items': {
+                    'accepted_count': 0,
+                    'rejected_count': 0,
+                    'submitted_count': 1
+                }
+            },
+            {
+                'name': "EML4-ALK T1151INST",
+                'id': 173,
+                'evidence_items': {
+                    'accepted_count': 4,
+                    'rejected_count': 0,
+                    'submitted_count': 1
+                }
+            }
+        ],
+        'aliases': [
+            "ALK",
+            "NBLST3",
+            "CD246"
+        ],
+        'type': "gene"
+    }
+
+
+@pytest.fixture(scope='module')
+def dux4():
+    """Create DUX4 gene record."""
+    return {
+        'id': 34321,
+        'name': "DUX4",
+        'entrez_id': 100288687,
+        'description': "",
+        'variants': [
+            {
+                'name': "DUX4 FUSIONS",
+                'id': 524,
+                'evidence_items': {
+                    'accepted_count': 2,
+                    'rejected_count': 0,
+                    'submitted_count': 0
+                }
+            },
+            {
+                'name': "DUX4-IGH",
+                'id': 2589,
+                'evidence_items': {
+                    'accepted_count': 0,
+                    'rejected_count': 0,
+                    'submitted_count': 1
+                }
+            }
+        ],
+        'aliases': [
+            "DUX4",
+            "DUX4L"
+        ],
+        'type': "gene"
+    }
+
+
+def test_genes_dux4(dux4, genes):
+    """Test civic harvester works correctly for genes."""
+    for gene in genes:
+        if gene['id'] == 34321:
+            actual_dux4 = gene
+            break
+    assert actual_dux4.keys() == dux4.keys()
+    keys = dux4.keys()
+    for key in keys:
+        assert actual_dux4[key] == dux4[key]
+
+
+def test_genes_alk(alk, genes):
+    """Test civic harvester works correctly for genes."""
+    for gene in genes:
+        if gene['id'] == 1:
+            actual_alk = gene
+            break
+    assert actual_alk.keys() == alk.keys()
+    keys = alk.keys()
+    for key in keys:
+        assert actual_alk[key] == alk[key]
