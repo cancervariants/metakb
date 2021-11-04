289a93fa42cb8d537b7de6f323fcd0f23d4af1d7
korikuzma <korikuzma@gmail.com>
2021-11-04 15:46:06 -0400

    Add variation norm exception message to logging msg

--- metakb/normalizers.py
+++ metakb/normalizers.py
@@ -42,9 +42,9 @@ class VICCNormalizers:
                         normalizer_responses.append(variation_norm_resp)
                     if not self.variation_normalizer.normalize_handler.warnings:  # noqa: E501
                         return variation_norm_resp.dict(exclude_none=True)
-            except:  # noqa: E722
-                logger.warning("Variation Normalizer does not support: "
-                               f"{query}")
+            except Exception as e:  # noqa: E722
+                logger.warning(f"Variation Normalizer could not "
+                               f"normalize {query}: {e}")
         return None
 
     def normalize_gene(self, queries) -> Tuple[Optional[dict], Optional[str]]:
