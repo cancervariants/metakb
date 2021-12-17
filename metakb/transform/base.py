79826eb65bbadbf58d9b36689a0f236cdf0605cc
James Stevenson <james.stevenson@nationwidechildrens.org>
2021-12-17 15:27:09 -0500

    Generate prop IDs using hash instead of incrementing ID numbers

--- metakb/transform/base.py
+++ metakb/transform/base.py
@@ -1,15 +1,11 @@
 """A module for the Transform base class."""
-from typing import Dict, Tuple, Optional
+from typing import Dict, List
 import json
 import logging
 
-from neo4j.work.transaction import Transaction
+from ga4gh.core import sha512t24u
 
-from metakb.schemas import PropositionType, Predicate, PredictivePredicate, \
-    DiagnosticPredicate, PrognosticPredicate, PathogenicPredicate, \
-    FunctionalPredicate
 from metakb.normalizers import VICCNormalizers
-from metakb.query import QueryHandler
 
 logger = logging.getLogger('metakb')
 logger.setLevel(logging.DEBUG)
@@ -18,15 +14,13 @@ logger.setLevel(logging.DEBUG)
 class Transform:
     """A base class for transforming harvester data."""
 
-    def __init__(self, file_path: str, uri: str = "",
-                 credentials: Tuple[str, str] = ("", "")):
+    def __init__(self, file_path: str):
         """Initialize Transform base class.
 
         :param str file_path: Path to harvested json to transform
         """
         self.file_path = file_path
         self.vicc_normalizers = VICCNormalizers()
-        self.query_handler = QueryHandler(uri, credentials)
 
     def transform(self, *args, **kwargs) -> Dict[str, dict]:
         """Transform harvested data to the Common Data Model.
@@ -65,77 +59,11 @@ class Transform:
             propositions_documents_ix[dict_key_ix] += 1
         return index
 
-    def get_proposition_id(
-            self,
-            prop_type: PropositionType,
-            pred: Predicate,
-            variation_id: str = "",
-            disease_id: str = "",
-            therapy_id: str = "") -> Optional[str]:
-        """Retrieve ID for proposition if it exists, in order to avoid creating
-        duplicate proposition entries. Should be passed as callback to Neo4j
-        read_transcation API.
-        :param PropositionType prop_type: type of proposition
-        :param Predicate pred: value of predicate
-        :param str variation_id: VRS ID
-        :param str disease_id: normalized disease ID
-        :param str therapy_id: normalized therapy ID
-        :return: complete proposition ID (eg `'proposition:182'`) or None
+    def get_proposition_ID_hash(self, concept_ids: List[str]) -> str:
+        """Get hash for proposition ID values.
+        :param List[str] concept_ids: all concept IDs relevant to the
+        proposition (therapies, variations, diseases). Order irrelevant.
+        :return: proposition ID as an SHA-512 hash of the IDs
         """
-        if (prop_type == PropositionType.PREDICTIVE
-                and not isinstance(pred, PredictivePredicate)) \
-            or (prop_type == PropositionType.DIAGNOSTIC
-                and not isinstance(pred, DiagnosticPredicate)) \
-            or (prop_type == PropositionType.PROGNOSTIC
-                and not isinstance(pred, PrognosticPredicate)) \
-            or (prop_type == PropositionType.PATHOGENIC
-                and not isinstance(pred, PathogenicPredicate)) \
-            or (prop_type == PropositionType.FUNCTIONAL
-                and not isinstance(pred, FunctionalPredicate)):
-            logger.error(
-                f"{prop_type} in query conflicts with {pred}"
-            )
-            return None
-        with self.query_handler.driver.session() as session:
-            params = {
-                "prop_type": prop_type,
-                "pred": pred,
-                "normalized_variation": variation_id,
-                "normalized_disease": disease_id,
-                "normalized_therapy": therapy_id
-            }
-            response = session.read_transaction(
-                self.query_handler._get_propositions,
-                **params
-            )
-            if len(response) < 1:
-                return None
-            elif len(response) > 1:
-                logger.warning(f"Found >1 propositions matching {params}")
-                return None
-            else:
-                return response[0].get("id")
-
-    def get_next_proposition_ID(self) -> int:
-        """Get next unused proposition ID number.
-        :return: integer corresponding to next available prop ID number.
-        Returns 1 if no propositions are found.
-        """
-        def _run_ix_query(tx: Transaction):
-            query = """MATCH (p:Proposition)
-            RETURN p.id
-            ORDER BY p.id
-            DESC LIMIT 1"""
-            result = tx.run(query)
-            if result is None:
-                return None
-            single = result.single()
-            if single is None:
-                return None
-            return single.get("p.id")
-        with self.query_handler.driver.session() as session:
-            result = session.read_transaction(_run_ix_query)
-        if result is None:
-            return 1
-        else:
-            return int(result.split(':')[1]) + 1
+        combined = "".join(sorted(concept_ids))
+        return sha512t24u(combined.encode())
