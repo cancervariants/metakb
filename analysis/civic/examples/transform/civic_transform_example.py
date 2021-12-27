4f05813d0c55da9cd37f6f695fdaa62397555861
James Stevenson <james.stevenson@nationwidechildrens.org>
2021-12-27 13:01:38 -0500

    Update examples

--- analysis/civic/examples/transform/civic_transform_example.py
+++ analysis/civic/examples/transform/civic_transform_example.py
@@ -1,7 +1,9 @@
 """Create an example json file for CIViC Transform."""
 import json
+from datetime import datetime as dt
+
+from metakb import PROJECT_ROOT, APP_ROOT, DATE_FMT
 from metakb.transform import CIViCTransform
-from metakb import PROJECT_ROOT, APP_ROOT
 
 
 def create_civic_example(civic_data):
@@ -71,8 +73,9 @@ def create_civic_example(civic_data):
         if d['id'] in documents:
             ex['documents'].append(d)
 
-    with open(f"{PROJECT_ROOT}/analysis/civic/examples/transform/"
-              f"civic_cdm.json", 'w+') as f2:
+    today = dt.strftime(dt.today(), DATE_FMT)
+    with open(PROJECT_ROOT / "analysis" / "civic" / "examples" /  # noqa: W504
+              "transform" / f"civic_cdm_{today}.json", 'w+') as f2:
         json.dump(ex, f2, indent=4)
 
 
