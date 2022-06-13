26707880ba3e21c45f67c85a65373b07e58d76e7
Maria Nakhoul <31465978+marianakhoul@users.noreply.github.com>
2022-06-12 21:58:26 -0400

    Update oncokb.py

--- metakb/harvesters/oncokb.py
+++ metakb/harvesters/oncokb.py
@@ -1,1 +1,8 @@
+"""A module for the OncoKB harvester."""
+import logging
+from typing import Optional
 
+from metakb.harvesters.base import Harvester
+
+logger = logging.getLogger('metakb.harvesters.oncokb')
+logger.setLevel(logging.DEBUG)
