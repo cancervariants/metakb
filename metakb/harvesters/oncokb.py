91dc73dc9f66e6b2d0475f2ada51b7ce6b92c633
Maria Nakhoul <31465978+marianakhoul@users.noreply.github.com>
2022-06-12 22:12:54 -0400

    Update oncokb.py

--- metakb/harvesters/oncokb.py
+++ metakb/harvesters/oncokb.py
@@ -6,3 +6,27 @@ from metakb.harvesters.base import Harvester
 
 logger = logging.getLogger('metakb.harvesters.oncokb')
 logger.setLevel(logging.DEBUG)
+
+class OncoKBHarvester(Harvester):
+  """A class for the OncoKB harvester."""
+  
+  def harvest(self, filename: Optional[str] = None):
+      """
+        Retrieve and store evidence, gene, variant, and assertion
+        records from OncoKB in composite and individual JSON files.
+        
+        :param Optional[str] filename: File name for composite json
+        :return: `True` if operation was successful, `False` otherwise.
+        :rtype: bool
+      """
+      try:
+        if not json_created:
+                logger.error('OncoKB Harvester was not successful.')
+                return False
+      except Exception as e:  # noqa: E722
+        logger.error(f'OncoKB Harvester was not successful: {e}')
+        return False
+      else:
+        logger.info('OncoKB Harvester was successful.')
+        return True
+ 
