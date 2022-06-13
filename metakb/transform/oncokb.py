6d84bfeff9118ae7db7f9aa86fd4edb0d7ac9913
Maria Nakhoul <31465978+marianakhoul@users.noreply.github.com>
2022-06-12 22:52:26 -0400

    Update oncokb.py

--- metakb/transform/oncokb.py
+++ metakb/transform/oncokb.py
@@ -1,1 +1,21 @@
+"""A module to transform OncoKB"""
+from typing import Optional, Dict, List, Set
+from pathlib import Path
+import logging
 
+from ga4gh.vrsatile.pydantic.vrsatile_models import VariationDescriptor, \
+    Extension, Expression, GeneDescriptor, ValueObjectDescriptor
+
+from metakb.transform.base import Transform
+import metakb.schemas as schemas
+
+
+logger = logging.getLogger('metakb.transform.civic')
+logger.setLevel(logging.DEBUG)
+
+class OncoKBTransform(Transform):
+    """A class for transforming OncoKB to the common data model."""
+    
+     async def transform(self):
+        """Transform OncoKB harvested json to common data model."""
+        
