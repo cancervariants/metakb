61044d945f87c20937f452740e3a16c06d5c4160
Zachary Flohr <zachary.flohr@case.edu>
2022-11-30 13:29:30 -0500

    Update names of envars storing normalizer paths in shell script

--- discover_normalizer_paths.sh
+++ discover_normalizer_paths.sh
@@ -3,18 +3,19 @@
 # Export useful paths for data dependencies
 #
 python3 -c '
-from therapy import PROJECT_ROOT as THERAPY_PROJECT_ROOT
+from therapy import APP_ROOT as THERAPY_PROJECT_ROOT
 from disease import PROJECT_ROOT as DISEASE_PROJECT_ROOT
-from gene import PROJECT_ROOT as GENE_PROJECT_ROOT
-from variant import SEQREPO_DATA_PATH, TRANSCRIPT_MAPPINGS_PATH
-import variant
+from gene import APP_ROOT as GENE_PROJECT_ROOT
+from gene import SEQREPO_DATA_PATH
+from variation import AMINO_ACID_PATH
+import variation
 
 print(f"""
 export THERAPY_PROJECT_ROOT={THERAPY_PROJECT_ROOT}
 export DISEASE_PROJECT_ROOT={DISEASE_PROJECT_ROOT}
 export GENE_PROJECT_ROOT={GENE_PROJECT_ROOT}
 export SEQREPO_DATA_PATH={SEQREPO_DATA_PATH}
-export TRANSCRIPT_MAPPINGS_PATH={TRANSCRIPT_MAPPINGS_PATH}
-export VARIANT_PROJECT_ROOT={variant.__path__[0]}
+export AMINO_ACID_PATH={AMINO_ACID_PATH}
+export VARIATION_PROJECT_ROOT={variation.__path__[0]}
 """)
 '
