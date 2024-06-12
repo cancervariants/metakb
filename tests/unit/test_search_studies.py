9c0c2cec6d4cfcefe4c621966f3fc3c3f1759976
James Stevenson <james.stevenson@nationwidechildrens.org>
2024-06-12 11:30:50 -0400

    fix: use latest minor civicpy release to get correct schema (#354)
    close #353 
    
    update tests accordingly

--- tests/unit/test_search_studies.py
+++ tests/unit/test_search_studies.py
@@ -4,7 +4,6 @@ import pytest
 
 from metakb.query import QueryHandler
 from metakb.schemas.api import SearchStudiesService
-from metakb.schemas.app import SourceName
 
 
 @pytest.fixture(scope="module")
@@ -74,9 +73,6 @@ async def test_civic_eid2997(query_handler, civic_eid2997_study, assertion_check
     # genomic query
     resp = await query_handler.search_studies(variation="7-55259515-T-G")
     find_and_check_study(resp, civic_eid2997_study, assertion_checks)
-    # At the moment, MOA cannot be queried via related genomic queries
-    # only civic stores genomic members
-    assert not [s_id for s_id in resp.study_ids if s_id.startswith(SourceName.MOA)]
 
     resp = await query_handler.search_studies(therapy="ncit:C66940")
     find_and_check_study(resp, civic_eid2997_study, assertion_checks)
