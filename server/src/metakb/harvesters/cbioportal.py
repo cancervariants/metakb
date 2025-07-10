ca89a7183bec98965f972cb971771eae68620713
Heather Costello <heather.costello@nationwidechildrens.org>
2025-07-10 10:45:43 -0400

    add Heather code to harvester

--- server/src/metakb/harvesters/cbioportal.py
+++ server/src/metakb/harvesters/cbioportal.py
@@ -2,6 +2,46 @@ from metakb.harvesters.base import Harvester, _HarvestedData
 import logging
 import pandas as pd
 
+
+#################HEATHER CODE################
+# imports
+
+import pandas as pd
+import requests
+import tarfile
+import os
+import numpy
+
+# dl_dir = "/Users/costellh/repos/metakb/hmc_notebooks/"
+
+
+# download files
+
+# download data 
+study = "es_dfarber_broad_2014"
+url = f"https://cbioportal-datahub.s3.amazonaws.com/{study}.tar.gz"
+output_path = f"{study}}.tar.gz"
+query_parameters = {"downloadformat": "tar.gz"}
+
+response = requests.get(url, stream=True, params=query_parameters)
+print(response.status_code)
+
+with open (output_path, mode="wb") as file: 
+    for chunk in response.iter_content(chunk_size=8192):
+        file.write(chunk)
+    print(f"Downloaded {output_path}")
+
+#TODO: softcode this
+extract_dir = f"{study}}_extracted"
+os.makedirs(extract_dir, exist_ok=True)
+
+with tarfile.open(output_path, mode="r:gz") as tar:
+    tar.extractall(path=extract_dir)
+
+print(f"Extracted to: {extract_dir}")
+#############END HEATHER CODE################
+
+
 logger = logging.getLogger(__name__)
 
 FILE_PATH = 'data/cbioportal' # TODO: Update to proper intended data path
