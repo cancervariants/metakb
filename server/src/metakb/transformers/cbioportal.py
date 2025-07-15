def1309c8a3f064fd299ef8cdb7b50fc7a4ca501
Cannon <mcannon068nw@gmail.com>
2025-07-15 14:48:41 -0400

    more comments + psuedocode for where functions from notebook will live, thoughts on how code should look eventually

--- server/src/metakb/transformers/cbioportal.py
+++ server/src/metakb/transformers/cbioportal.py
@@ -55,12 +55,37 @@ class cBioportalTransformer(Transformer):
 
     def transform(self, harvested_data):
 
+        # for harvested_data_obj in harvested_data:
         self.variants = pd.DataFrame(harvested_data.variants).filter(MUT_HEADERS)
         self.patients = pd.DataFrame(harvested_data.patients).filter(PATIENT_HEADERS)
         self.samples = pd.DataFrame(harvested_data.samples).filter(SAMPLE_HEADERS)
         self.metadata = pd.DataFrame(harvested_data.metadata)
 
         # TODO: Heather's methods for data cleanup, normalization, and formating to common data model goes here
-        # TODO: civic.py transformer may have some logic to reuse here
+
+        # def _chr23_male():
+        #   relevant code
+        # 
+        # def _add_cols_chrom_23_male():
+        #   relevant code 
+        # 
+        # def _check_for_x_variant():
+        #   relevant code
+        #
+        # def _check_for_y_variant():
+        #   relevant code
+        #
+        # def _test_tokenization():
+        #   relevant code
+
+        # self.data = _create_combined_df(self,relevant data frames)
+        # self.data = _chr23_male(self,relevant data frame)
+        
+
+        # TODO: Method's for mapping cleaned up data to Common Data Model format
+        # civic.py transformer may have some logic to reuse here
+        # test fixture is the end result for a single entry
+        #   
+        # TODO: Output one CDM per study or one CDM total?
 
         pass
