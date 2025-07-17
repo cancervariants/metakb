cfd1d4d3d5a8f3f0c6dbe5d75013e1e517d97d97
Anastasia Bratulin <anastasiasmith1221@gmail.com>
2025-07-17 15:58:26 -0400

    started moving info from csv to json based on VariantTherapeuticResponseProposition template (type, predicate, description, geneContextQualifier, and objectTherapeutic

---
+++ server/src/metakb/harvesters/poda.py
@@ -0,0 +1,13 @@
+from metakb.harvesters.base import Harvester, _HarvestedData
+import logging
+import pandas as pd
+import requests
+
+
+data = "pediatric-oncology-drug-approvals"
+url = https://www.fda.gov/about-fda/oncology-center-excellence/{data}
+output_path = f"{data}.tar.gz"
+query_parameters = {"downloadformat": "tar.gz"}
+
+response = requests.get(url, stream=True, params=query_parameters)
+print(response.status_code)
