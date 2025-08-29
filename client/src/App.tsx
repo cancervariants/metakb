7b4c0c22790f7bf1eb2abc2393f4fa6bf5c3256a
Katie Stahl <kathryn.stahl@nationwidechildrens.org>
2025-08-29 13:03:32 -0400

    feat: add frontend checks to precommit

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,6 +6,8 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
+
+  
   return (
     <>
       <div>
@@ -16,7 +18,9 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React
+        
+      </h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
         <p>
