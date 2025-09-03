c9870f0e699d096308585d4cb4bd40bfee34c9e1
James Stevenson <james.stevenson@nationwidechildrens.org>
2025-09-03 14:15:10 -0400

    cleanup

--- server/src/metakb/load_data.py
+++ server/src/metakb/load_data.py
@@ -7,9 +7,13 @@ from pathlib import Path
 
 from ga4gh.core.models import Extension, MappableConcept
 from ga4gh.va_spec.base import (
+    ConditionSet,
     MembershipOperator,
     Statement,
     TherapyGroup,
+    VariantDiagnosticProposition,
+    VariantPrognosticProposition,
+    VariantTherapeuticResponseProposition,
 )
 from neo4j import Driver, ManagedTransaction
 
@@ -32,6 +36,38 @@ def _has_normalize_failure(extensions: list[Extension] | None) -> bool:
     return False
 
 
+def _is_loadable_condition(
+    condition: MappableConcept | ConditionSet, statement_id: str
+) -> bool:
+    """Check if condition or condition set can be loaded into DB
+
+    :param condition: condition or conditionset to check
+    :param statement_id: ID to use in logging failure message
+    :return: whether condition is loadable
+    :raise ValueError: if unsupported type of condition (eg IRI ref) is provided
+    """
+    if isinstance(condition, MappableConcept):
+        if _has_normalize_failure(condition.extensions):
+            _logger.info(
+                "%s could not be loaded because condition failed to normalize: %s",
+                statement_id,
+                condition,
+            )
+            return False
+    elif isinstance(condition, ConditionSet):
+        for member_condition in condition.conditions:
+            if _has_normalize_failure(member_condition.extensions):
+                _logger.info(
+                    "%s could not be loaded because condition in ConditionSet failed to normalize: %s",
+                    statement_id,
+                    condition,
+                )
+                return False
+    else:
+        raise ValueError  # noqa: TRY004
+    return True
+
+
 def is_loadable_statement(statement: Statement) -> bool:
     """Check whether statement can be loaded to DB
 
@@ -45,13 +81,14 @@ def is_loadable_statement(statement: Statement) -> bool:
         not simply referenced as IRIs
     :return: whether statement can be loaded given current data support policy
     :raise NotImplementedError: if unsupported proposition type is provided
+    :raise ValueError: if unsupported type used for therapeutic (eg string IRI)
     """
     success = True
     if evidence_lines := statement.hasEvidenceLines:
         for evidence_line in evidence_lines:
             for evidence_item in evidence_line.hasEvidenceItems:
                 if not is_loadable_statement(evidence_item):
-                    _logger.debug(
+                    _logger.info(
                         "%s could not be loaded because %s is not supported",
                         statement.id,
                         evidence_item.id,
@@ -59,51 +96,41 @@ def is_loadable_statement(statement: Statement) -> bool:
                     success = False
     proposition = statement.proposition
     if not proposition.subjectVariant.constraints:
-        _logger.debug(
+        _logger.info(
             "%s could not be loaded because subject variant object lacks constraints: %s",
             statement.id,
             proposition.subjectVariant,
         )
         success = False
-    proposition_type = proposition.type
-    if proposition_type == "VariantTherapeuticResponseProposition":
-        if _has_normalize_failure(proposition.conditionQualifier.root.extensions):
-            _logger.debug(
-                "%s could not be loaded because condition failed to normalize: %s",
-                statement.id,
-                proposition.conditionQualifier.root,
-            )
+    if isinstance(proposition, VariantTherapeuticResponseProposition):
+        if not _is_loadable_condition(
+            proposition.conditionQualifier.root, statement.id
+        ):
             success = False
-        if therapeutic := proposition.objectTherapeutic:
-            if isinstance(therapeutic.root, MappableConcept):
-                if _has_normalize_failure(therapeutic.root.extensions):
-                    _logger.debug(
-                        "%s could not be loaded because drug failed to normalize: %s",
+        therapeutic = proposition.objectTherapeutic.root
+        if isinstance(therapeutic, MappableConcept):
+            if _has_normalize_failure(therapeutic.extensions):
+                _logger.info(
+                    "%s could not be loaded because drug failed to normalize: %s",
+                    statement.id,
+                    therapeutic,
+                )
+                success = False
+        elif isinstance(therapeutic, TherapyGroup):
+            for drug in therapeutic.therapies:
+                if _has_normalize_failure(drug.extensions):
+                    _logger.info(
+                        "%s could not be loaded because drug in therapygroup failed to normalize: %s",
                         statement.id,
-                        therapeutic.root,
+                        drug,
                     )
                     success = False
-            elif isinstance(therapeutic.root, TherapyGroup):
-                for drug in therapeutic.root.therapies:
-                    if _has_normalize_failure(drug.extensions):
-                        _logger.debug(
-                            "%s could not be loaded because drug in therapygroup failed to normalize: %s",
-                            statement.id,
-                            drug,
-                        )
-                        success = False
-            else:
-                raise TypeError
-    elif proposition_type in (
-        "VariantDiagnosticProposition",
-        "VariantPrognosticProposition",
+        else:
+            raise ValueError  # noqa: TRY004
+    elif isinstance(
+        proposition, VariantDiagnosticProposition | VariantPrognosticProposition
     ):
-        if _has_normalize_failure(proposition.objectCondition.root.extensions):
-            _logger.debug(
-                "%s could not be loaded because condition failed to normalize: %s",
-                statement.id,
-                proposition.objectCondition.root,
-            )
+        if not _is_loadable_condition(proposition.objectCondition.root, statement.id):
             success = False
     else:
         msg = f"Unsupported proposition type: {proposition.type}"
@@ -111,16 +138,16 @@ def is_loadable_statement(statement: Statement) -> bool:
     if proposition.geneContextQualifier and _has_normalize_failure(
         proposition.geneContextQualifier.extensions
     ):
-        _logger.debug(
+        _logger.info(
             "%s could not be loaded because gene failed to normalize: %s",
             statement.id,
             proposition.geneContextQualifier,
         )
         success = False
     if success:
-        _logger.debug("Success. %s can be loaded.", statement.id)
+        _logger.info("Success. %s can be loaded.", statement.id)
     else:
-        _logger.debug("Failure. %s cannot be loaded.", statement.id)
+        _logger.info("Failure. %s cannot be loaded.", statement.id)
     return success
 
 
