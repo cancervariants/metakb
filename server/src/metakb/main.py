a67fe360e7f5d7f7bcbe11771ce23f14191947b7
James Stevenson <james.stevenson@nationwidechildrens.org>
2025-10-02 09:22:20 -0400

    feat: add deployment scripts and mount static files in backend (#576)
    close #475 
    
    * Add elasticbeanstalk deployment scripts for frontend build
    * Update server env to include a default 'local' setting
    * Refactor routes and dependencies into a `/restapi` subdirectory. As we
    build out entity-specific endpoints I envision us adding a new
    `server/src/metakb/restapi/resources.py` module or something to that
    effect
    * Add route/etc to provide static file service
    * Use CORS configs copy/pasted from fusion-curation. I hope this means
    it'll work, but who knows how correct it is. I'd like to make a separate
    ticket to double check it from a security perspective afterwards.
    * Add Makefile w/ targets for generating zips for manual deploy in
    elasticbeanstalk (hopefully not something we ever need to do again, but
    it was helpful while writing this and I figured I shouldn't throw it
    away) and for regenerating `requirements.txt`
    
    The ElasticBeanstalk configs should be updated to provide a `METAKB_ENV`
    variable with either `"dev"`, `"staging"`, or `"prod"` as the value
    (lower-case). I think we should also set `METAKB_DEBUG=true` in staging
    and dev environments.

--- server/src/metakb/main.py
+++ server/src/metakb/main.py
@@ -1,43 +1,32 @@
-"""Main application for FastAPI."""
+"""Main application entrypoint."""
 
 import logging
-from collections.abc import AsyncGenerator, Generator
+from collections.abc import AsyncGenerator
 from contextlib import asynccontextmanager
 from enum import Enum
-from time import perf_counter
-from typing import Annotated
+from pathlib import Path
 
-from fastapi import Depends, FastAPI, HTTPException, Query, Request
-from ga4gh.va_spec.base import (
-    VariantDiagnosticProposition,
-    VariantPrognosticProposition,
-    VariantTherapeuticResponseProposition,
-)
+from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 from metakb import __version__
 from metakb.config import get_config
 from metakb.normalizers import ViccNormalizers
-from metakb.repository.base import AbstractRepository
 from metakb.repository.neo4j_repository import (
-    Neo4jRepository,
     get_driver,
 )
+from metakb.restapi.search import api_router as search_router
 from metakb.schemas.api import (
     METAKB_DESCRIPTION,
-    BatchSearchStatementsResponse,
-    SearchStatementsQuery,
-    SearchStatementsResponse,
     ServiceInfo,
-    ServiceMeta,
     ServiceOrganization,
     ServiceType,
 )
-from metakb.services.search import (
-    EmptySearchError,
-    batch_search_statements,
-    search_statements,
-)
 from metakb.utils import configure_logs
+
+_logger = logging.getLogger(__name__)
 
 
 @asynccontextmanager
@@ -55,16 +44,7 @@ async def lifespan(app: FastAPI) -> AsyncGenerator:
     driver.close()
 
 
-def get_repository() -> Generator[AbstractRepository, None, None]:
-    """Provide repository factory for REST API route dependency injection
-
-    :return: generator yielding a repository instance. Performs cleanup when route
-        invocation concludes.
-    """
-    session = app.state.driver.session()
-    repository = Neo4jRepository(session)
-    yield repository
-    session.close()
+API_PREFIX = "/api/v2"
 
 
 app = FastAPI(
@@ -80,7 +60,7 @@ app = FastAPI(
         "email": "Alex.Wagner@nationwidechildrens.org",
         "url": "https://www.nationwidechildrens.org/specialties/institute-for-genomic-medicine/research-labs/wagner-lab",
     },
-    docs_url="/api/v2",
+    docs_url=API_PREFIX,
     openapi_url="/api/v2/openapi.json",
     swagger_ui_parameters={"tryItOutEnabled": True},
     lifespan=lifespan,
@@ -94,8 +74,11 @@ class _Tag(str, Enum):
     SEARCH = "Search"
 
 
+app.include_router(search_router, tags=[_Tag.SEARCH], prefix=API_PREFIX)
+
+
 @app.get(
-    "/service-info",
+    f"{API_PREFIX}/service-info",
     summary="Get basic service information",
     description="Retrieve service metadata, such as versioning and contact info. Structured in conformance with the [GA4GH service info API specification](https://www.ga4gh.org/product/service-info/)",
     tags=[_Tag.META],
@@ -109,155 +92,29 @@ def service_info() -> ServiceInfo:
     )
 
 
-search_stmts_summary = (
-    "Get nested statements from queried concepts that match all conditions provided."
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost", "http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
-search_stmts_descr = (
-    "Return nested statements that match the intersection of queried concepts. For "
-    "example, if `variation` and `therapy` are provided, will return all statements "
-    "that have both the provided `variation` and `therapy`."
-)
-v_description = "Variation (subject) to search. Can be free text or VRS Variation ID."
-d_description = "Disease (object qualifier) to search"
-t_description = "Therapy (object) to search"
-g_description = "Gene to search"
-s_description = "Statement ID to search."
-start_description = "The index of the first result to return. Use for pagination."
-limit_description = "The maximum number of results to return. Use for pagination."
-
-
-@app.get(
-    "/api/v2/search/statements",
-    summary=search_stmts_summary,
-    response_model_exclude_none=True,
-    description=search_stmts_descr,
-    tags=[_Tag.SEARCH],
-)
-async def get_statements(
-    request: Request,
-    repository: Annotated[AbstractRepository, Depends(get_repository)],
-    variation: Annotated[str | None, Query(description=v_description)] = None,
-    disease: Annotated[str | None, Query(description=d_description)] = None,
-    therapy: Annotated[str | None, Query(description=t_description)] = None,
-    gene: Annotated[str | None, Query(description=g_description)] = None,
-    statement_id: Annotated[str | None, Query(description=s_description)] = None,
-    start: Annotated[int, Query(description=start_description, ge=0)] = 0,
-    limit: Annotated[int | None, Query(description=limit_description, ge=0)] = None,
-) -> SearchStatementsResponse:
-    """Get nested statements from queried concepts that match all conditions provided.
-
-    For example, if `variation` and `therapy` are provided, will return all statements
-    that have both the provided `variation` and `therapy`.
-    """
-    start_time = perf_counter()
-    normalizer: ViccNormalizers = request.app.state.normalizer
-    try:
-        search_results = await search_statements(
-            repository,
-            normalizer,
-            variation,
-            disease,
-            therapy,
-            gene,
-            statement_id,
-            start,
-            limit,
-        )
-    except EmptySearchError as e:
-        raise HTTPException(
-            status_code=422,
-            detail="At least one search parameter (variation, disease, therapy, gene, statement_id) must be provided.",
-        ) from e
-
-    mapped_terms = {term.term_type.value: term for term in search_results.search_terms}
-
-    # group statements
-    grouped_statements = {
-        "therapeutic_statements": {},
-        "diagnostic_statements": {},
-        "prognostic_statements": {},
-    }
-    statement_ids = []
-    for statement in search_results.statements:
-        statement_ids.append(statement.id)
-        variant_id = statement.proposition.subjectVariant.id
-        predicate = statement.proposition.predicate
-        if isinstance(statement.proposition, VariantTherapeuticResponseProposition):
-            key = f"{variant_id}|{statement.proposition.conditionQualifier.root.id}|{statement.proposition.objectTherapeutic.root.id}|{predicate}"
-            grouped_statements["therapeutic_statements"].setdefault(key, []).append(
-                statement
-            )
-        elif isinstance(statement.proposition, VariantDiagnosticProposition):
-            key = f"{variant_id}|{statement.proposition.objectCondition.root.id}|{predicate}"
-            grouped_statements["diagnostic_statements"].setdefault(key, []).append(
-                statement
-            )
-        elif isinstance(statement.proposition, VariantPrognosticProposition):
-            key = f"{variant_id}|{statement.proposition.objectCondition.root.id}|{predicate}"
-            grouped_statements["prognostic_statements"].setdefault(key, []).append(
-                statement
-            )
-        else:
-            msg = f"Unrecognized proposition type `{type(statement.proposition)}` in {statement}"
-            raise ValueError(msg)  # noqa: TRY004
-
-    end_time = perf_counter()
-    return SearchStatementsResponse(
-        query=SearchStatementsQuery(**mapped_terms),
-        start=start,
-        limit=limit,
-        service_meta_=ServiceMeta(),
-        statement_ids=statement_ids,
-        duration_s=end_time - start_time,
-        **grouped_statements,
+BUILD_DIR = Path(__file__).parent / "build"
+try:
+    app.mount("/assets", StaticFiles(directory=BUILD_DIR / "assets"), name="assets")
+except RuntimeError:
+    _logger.warning(
+        "Unable to locate static frontend files under path `%s`. Proceeding without defining `get_client()`"
     )
+    _logger.debug("Build dir contents: %s", BUILD_DIR.rglob("*"))
+else:
+    templates = Jinja2Templates(directory=BUILD_DIR.as_posix())
 
+    @app.get("/{full_path:path}", include_in_schema=False)
+    async def get_client(request: Request, full_path: str):  # noqa: ANN201, ARG001
+        """Serve static client files
 
-_batch_descr = {
-    "summary": "Get nested statements for all provided variations.",
-    "description": "Return nested statements associated with any of the provided variations.",
-    "arg_variations": "Variations (subject) to search. Can be free text or VRS variation ID.",
-    "arg_start": "The index of the first result to return. Use for pagination.",
-    "arg_limit": "The maximum number of results to return. Use for pagination.",
-}
-
-
-@app.get(
-    "/api/v2/batch_search/statements",
-    summary=_batch_descr["summary"],
-    response_model_exclude_none=True,
-    description=_batch_descr["description"],
-    tags=[_Tag.SEARCH],
-)
-async def batch_get_statements(
-    request: Request,
-    repository: Annotated[AbstractRepository, Depends(get_repository)],
-    variations: Annotated[
-        list[str] | None,
-        Query(description=_batch_descr["arg_variations"]),
-    ] = None,
-    start: Annotated[int, Query(description=_batch_descr["arg_start"])] = 0,
-    limit: Annotated[int | None, Query(description=_batch_descr["arg_limit"])] = None,
-) -> BatchSearchStatementsResponse:
-    """Fetch all statements associated with `any` of the provided variations."""
-    start_time = perf_counter()
-
-    normalizer: ViccNormalizers = request.app.state.normalizer
-    try:
-        results = await batch_search_statements(
-            repository, normalizer, variations, start, limit
-        )
-    except EmptySearchError as e:
-        raise HTTPException(
-            status_code=422,
-            detail="At least one search parameter must be provided, but no variations values have been given.",
-        ) from e
-    end_time = perf_counter()
-    return BatchSearchStatementsResponse(
-        search_terms=results.search_terms,
-        start=start,
-        limit=limit,
-        service_meta_=ServiceMeta(),
-        statements=results.statements,
-        duration_s=end_time - start_time,
-    )
+        This route should be the VERY LAST thing associated with the Fastapi `app` object,
+        because it should shadow all unclaimed paths
+        """
+        return templates.TemplateResponse("index.html", {"request": request})
