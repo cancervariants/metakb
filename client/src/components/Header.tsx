390266672a0000949e0c71b8159bfa2c058eb683
James Stevenson <james.stevenson@nationwidechildrens.org>
2025-10-10 18:36:42 -0400

    feat: add version stamp to page (#597)
    close #591. This one is a little undercooked, but, like... progress?
    
    Caveats
    
    * This is not information that should fire at every page request. And
    yet! So uh maybe that should be fixed, if possible
    * The footer doesn't exist yet. So I put it in the header. (I did not
    bother with any styling because this is presumably a temporary location,
    but honestly it doesn't look that bad)
    * The version looks funky because of how setuptools-scm designates
    versioning. Basically, if you are in a commit that isn't tagged with a
    release version, then it just walks back to the most recent tag and then
    adds extra stuff to indicate how many additional commits have been made,
    as well as a truncation of the current commit hash. So when we tag
    actual releases and deploy them, it'll just say "2.1.0" or whatever. We
    could also just do a split and cut off everything including and after
    the "+".
    
    <img width="415" height="153" alt="Screenshot 2025-10-08 at 4 28 57 PM"
    src="https://github.com/user-attachments/assets/d2984e48-d1d5-461b-9a76-555257689d07"
    />

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,7 +1,23 @@
+import { useEffect, useState } from 'react'
 import { AppBar, Typography } from '@mui/material'
 import { Link } from 'react-router-dom'
 
 const Header = () => {
+  const [version, setVersion] = useState('')
+
+  useEffect(() => {
+    const run = async () => {
+      const url = '/cv-api/api/v2/service-info'
+      const res = await fetch(url, {
+        headers: { 'Content-Type': 'application/json' },
+      })
+      if (!res.ok) throw new Error(`Service info API request failed: ${res.status}`)
+      const data = await res.json()
+      setVersion(data.version)
+    }
+    run()
+  }, [])
+
   return (
     <AppBar position="static" color="header" sx={{ padding: 2 }}>
       <Link to="/">
@@ -9,6 +25,7 @@ const Header = () => {
           MetaKB
         </Typography>
       </Link>
+      <Typography>{version ? `v${version}` : ''}</Typography>
     </AppBar>
   )
 }
