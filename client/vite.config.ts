09240c868e713773233defd3ad63a202f2d2ce30
Katie Stahl <kathryn.stahl@nationwidechildrens.org>
2025-10-13 14:40:44 -0400

    fix: remove rewrite

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -8,7 +8,6 @@ export default defineConfig({
       '/api': {
         target: 'http://localhost:8000',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
       }
     }
   }
