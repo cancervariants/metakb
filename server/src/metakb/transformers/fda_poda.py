1a65d77a00c539ea97d3e26a1f772be05f5a1c49
James Stevenson <james.stevenson@nationwidechildrens.org>
2025-11-10 16:38:16 -0500

    stash

--- server/src/metakb/transformers/fda_poda.py
+++ server/src/metakb/transformers/fda_poda.py
@@ -1,9 +1,17 @@
 from pathlib import Path
+from typing import ClassVar
 
 from ga4gh.cat_vrs.models import CategoricalVariant
+from ga4gh.va_spec.base import Statement
+from pydantic import BaseModel
 
 from metakb.normalizers import ViccNormalizers
-from metakb.transformers.base import MethodId, Transformer, _HarvestedData
+from metakb.transformers.base import (
+    MethodId,
+    Transformer,
+    _HarvestedData,
+    _TransformedRecordsCache,
+)
 
 
 class CivicHarvestedData(_HarvestedData):
@@ -15,27 +23,33 @@ class CivicHarvestedData(_HarvestedData):
     assertions: list[dict]
 
 
-class CivicTransformer(Transformer):
-    """A class for transforming CIViC to the common data model."""
+class _FdaTransformedCache(_TransformedRecordsCache):
+    """Create model for caching FDA PODA data"""
 
+    categorical_variants: ClassVar[dict[str, CategoricalVariant]] = {}
+    evidence: ClassVar[
+        dict[
+            str,
+            Statement,
+        ]
+    ] = {}
+
+
+class FdaPodaTransformer(Transformer):
     def __init__(
         self,
         data_dir: Path | None = None,
         harvester_path: Path | None = None,
         normalizers: ViccNormalizers | None = None,
     ) -> None:
-        """Initialize CIViC Transformer class.
-
-        :param data_dir: Path to source data directory
-        :param harvester_path: Path to previously harvested CIViC data
-        :param normalizers: normalizer collection instance
-        """
         super().__init__(
             data_dir=data_dir, harvester_path=harvester_path, normalizers=normalizers
         )
-
         # Method will always be the same
         self.processed_data.methods = [
             self.methods_mapping[MethodId.FDA_APPROV_SOP.value]
         ]
         self._cache = self._create_cache()
+
+    def _create_cache(self) -> _FdaTransformedCache:
+        return _FdaTransformedCache()
