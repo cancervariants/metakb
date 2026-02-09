31393cf385c9167067f69cbb171970c4b23113cd
Heather Costello <heather.costello@nationwidechildrens.org>
2026-02-09 15:49:43 -0500

    add new files for st jude 2015 study

---
+++ server/src/metakb/transformers/cbioportal/transformer_all_stjude_2015.py
@@ -0,0 +1,69 @@
+from os import environ
+
+environ["AWS_ACCESS_KEY_ID"] = "dummy"
+environ["AWS_SECRET_ACCESS_KEY"] = "dummy"
+environ["AWS_SESSION_TOKEN"] = "dummy"
+
+import logging
+
+from metakb.transformers.cbioportal.base import cBioportalStudyTransformer
+
+_logger = logging.getLogger(__name__)
+
+
+MUT_HEADERS = [
+    "Hugo_Symbol",
+    "Entrez_Gene_Id",
+    "Center",
+    "NCBI_Build",
+    "Chromosome",
+    "Start_Position",
+    "End_Position",
+    "Consequence",
+    "Variant_Classification",
+    "Variant_Type",
+    "Reference_Allele",
+    "Tumor_Seq_Allele2",
+    "Tumor_Sample_Barcode",
+    "HGVSc",
+    "HGVSp",
+    "HGVSp_Short",
+    "Transcript_ID",
+    "RefSeq",
+    "Protein_position",
+    "Codons",
+    "Amino_Acid_Change",
+]
+
+PATIENT_HEADERS = ["PATIENT_ID", "AGE", "SEX", "RACE"]
+
+SAMPLE_HEADERS = [
+    "PATIENT_ID",
+    "SAMPLE_ID",
+    "GENE_PANEL",
+    "ONCOTREE_CODE",
+    "CANCER_TYPE",
+    "CANCER_TYPE_DETAILED",
+    "TMB_NONSYNONYMOUS",
+]
+
+
+class cBioportalTransformer(cBioportalStudyTransformer):
+    """Transformer for all_stjude_2015 study."""
+
+    def get_study_name(self) -> str:
+        return "all_stjude_2015"
+
+    def get_mut_headers(self) -> list[str]:
+        return MUT_HEADERS
+
+    def get_patient_headers(self) -> list[str]:
+        return PATIENT_HEADERS
+
+    def get_sample_headers(self) -> list[str]:
+        return SAMPLE_HEADERS
+
+    def get_sample_transformations(self) -> dict:
+        return {
+            "sequence_source": "GENE_PANEL"
+        }
