57d10d9523a1a966c15eecefee6b0a0a1bf36aca
Katie Stahl <kathryn.stahl@nationwidechildrens.org>
2026-02-13 09:09:16 -0500

    adjust padding

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -4,7 +4,7 @@ import metakbJrLogo from '../assets/metakbjr-logo.png'
 
 const Header = () => {
   return (
-    <AppBar position="static" color="header" sx={{ padding: 2 }}>
+    <AppBar position="static" color="header">
       <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
         <Box display="flex" alignItems="center" gap={2}>
           <Link to="/" style={{ textDecoration: 'none', color: 'inherit' }}>
@@ -12,7 +12,7 @@ const Header = () => {
               component="img"
               src={metakbJrLogo}
               alt="MetaKB Jr."
-              sx={{ height: 75, width: 'auto', display: 'block' }}
+              sx={{ height: 75, width: 'auto', display: 'block', padding: 1}}
             />
           </Link>
         </Box>
