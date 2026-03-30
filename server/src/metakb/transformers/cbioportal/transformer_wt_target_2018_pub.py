4c6dfc3d186bd13f438043e013896b8ae9c9ce69
Heather Costello <heather.costello@nationwidechildrens.org>
2026-03-30 14:26:12 -0400

    add wt_target_2018_pub transformer

---
+++ server/src/metakb/transformers/cbioportal/transformer_wt_target_2018_pub.py
@@ -0,0 +1,72 @@
+from os import environ
+
+environ["AWS_ACCESS_KEY_ID"] = "dummy"
+environ["AWS_SECRET_ACCESS_KEY"] = "dummy"
+environ["AWS_SESSION_TOKEN"] = "dummy"
+
+import logging
+
+from metakb.transformers.cbioportal.base import cBioportalStudyTransformer
+
+_logger = logging.getLogger(__name__)
+
+
+MUT_HEADERS = [
+    "Hugo_Symbol",
+    "Entrez_Gene_Id",
+    "Center",
+    "NCBI_Build",
+    "Chromosome",
+    "Start_Position",
+    "End_Position",
+    "Consequence",
+    "Variant_Classification",
+    "Variant_Type",
+    "Reference_Allele",
+    "Tumor_Seq_Allele2",
+    "Tumor_Sample_Barcode",
+    "Sequence_Source",
+    "HGVSc",
+    "HGVSp",
+    "HGVSp_Short",
+    "Transcript_ID",
+    "RefSeq",
+    "Protein_position",
+    "Codons",
+    "Amino_Acid_Change",
+]
+
+PATIENT_HEADERS = ["PATIENT_ID", "AGE", "SEX", "RACE"]
+
+SAMPLE_HEADERS = [
+    "PATIENT_ID",
+    "SAMPLE_ID",
+    "SAMPLE_CLASS",
+    "ONCOTREE_CODE",
+    "CANCER_TYPE",
+    "CANCER_TYPE_DETAILED",
+    "TMB_NONSYNONYMOUS",
+]
+
+
+class cBioportalTransformer(cBioportalStudyTransformer):
+    """Transformer for wt_target_2018_pub study."""
+
+    def get_study_name(self) -> str:
+        return "wt_target_2018_pub"
+
+    def get_mut_headers(self) -> list[str]:
+        return MUT_HEADERS
+
+    def get_patient_headers(self) -> list[str]:
+        return PATIENT_HEADERS
+
+    def get_sample_headers(self) -> list[str]:
+        return SAMPLE_HEADERS
+
+    def get_variant_transformations(self) -> dict:
+        return {
+            "additional_columns": {
+                "Sequence_Source": "No_data"
+            }
+        }
