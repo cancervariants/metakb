0df596b806073012662c442d9c8acf86f7589d10
hmcostello <52254475+hmcostello@users.noreply.github.com>
2026-03-31 10:46:00 -0400

    Study transformer 14 (#797)
    Transformer for study rt_target_2018_pub
    
    To run PL:
    
    [run_cbp_pl.py](https://github.com/user-attachments/files/26358279/run_cbp_pl.py)
    
    
    To test frequencies:
    
    [validate_frequencies.py](https://github.com/user-attachments/files/26358277/validate_frequencies.py)

---
+++ server/src/metakb/transformers/cbioportal/transformer_rt_target_2018_pub.py
@@ -0,0 +1,60 @@
+"""Transformer for the rt_target_2018_pub cBioPortal study."""
+
+from metakb.transformers.cbioportal.base import CBioPortalStudyTransformer
+
+MUT_HEADERS = [
+    "Hugo_Symbol",
+    "Entrez_Gene_Id",
+    "Center",
+    "NCBI_Build",
+    "Chromosome",
+    "Start_Position",
+    "End_Position",
+    "Consequence",
+    "Variant_Classification",
+    "Variant_Type",
+    "Reference_Allele",
+    "Tumor_Seq_Allele2",
+    "Tumor_Sample_Barcode",
+    "Sequence_Source",
+    "HGVSc",
+    "HGVSp",
+    "HGVSp_Short",
+    "Transcript_ID",
+    "RefSeq",
+    "Protein_position",
+    "Codons",
+    "Amino_Acid_Change",
+]
+
+PATIENT_HEADERS = ["PATIENT_ID", "AGE", "SEX", "RACE"]
+
+SAMPLE_HEADERS = [
+    "PATIENT_ID",
+    "SAMPLE_ID",
+    "SAMPLE_CLASS",
+    "ONCOTREE_CODE",
+    "CANCER_TYPE",
+    "CANCER_TYPE_DETAILED",
+    "TMB_NONSYNONYMOUS",
+]
+
+
+class CBioPortalTransformer(CBioPortalStudyTransformer):
+    """Transformer for rt_target_2018_pub study."""
+
+    def get_study_name(self) -> str:
+        """Return the study identifier."""
+        return "rt_target_2018_pub"
+
+    def get_mut_headers(self) -> list[str]:
+        """Return the list of mutation/variant column headers to keep."""
+        return MUT_HEADERS
+
+    def get_patient_headers(self) -> list[str]:
+        """Return the list of patient column headers to keep."""
+        return PATIENT_HEADERS
+
+    def get_sample_headers(self) -> list[str]:
+        """Return the list of sample column headers to keep."""
+        return SAMPLE_HEADERS
