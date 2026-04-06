b1b209a60f1701cd8804adb9d3d68601831f73cb
James Stevenson <james.stevenson@nationwidechildrens.org>
2026-04-06 18:38:35 -0400

    update todos

--- server/tests/unit/services/test_manage_data.py
+++ server/tests/unit/services/test_manage_data.py
@@ -4,8 +4,6 @@ from pathlib import Path
 import pytest
 from ga4gh.va_spec.base import Statement
 
-from metakb.services.manage_data import is_loadable_assertion
-
 
 @pytest.fixture(scope="session")
 def statements(test_data_dir: Path):
@@ -14,9 +12,5 @@ def statements(test_data_dir: Path):
     return {k: Statement(**v) for k, v in data.items()}
 
 
-@pytest.mark.ci_ok
-def test_is_loadable_statement(statements: dict[str, Statement]):
-    assert is_loadable_assertion(
-        statements["metakb.assertion:F-6C4CgAIyw3cf2zdxRVOVfe3L1GbHqa"]
-    )
-    assert not is_loadable_assertion(statements["moa.assertion:1"])
+def test_load_from_json():
+    pass  # TODO
--- server/tests/unit/transformers/test_methodology.py
+++ server/tests/unit/transformers/test_methodology.py
@@ -1,14 +1,14 @@
 def test_src_strength_to_vicc_code():
-    pass
+    pass  # TODO
 
 
 def test_initialize_assertion():
-    pass
+    pass  # TODO
 
 
 def test_merge_assertions():
-    pass
+    pass  # TODO
 
 
 def test_add_evidence_to_assertion():
-    pass
+    pass  # TODO
