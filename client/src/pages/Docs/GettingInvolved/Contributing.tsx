49110037e28c4742c5c5897762fd6a1c27ba1164
Matthew Cannon <mcannon068nw@gmail.com>
2026-05-19 13:35:12 -0400

    Contributing + ReportingIssues; pages filled in

--- client/src/pages/Docs/GettingInvolved/Contributing.tsx
+++ client/src/pages/Docs/GettingInvolved/Contributing.tsx
@@ -1,4 +1,4 @@
-import { Typography } from '@mui/material'
+import { Typography, Box } from '@mui/material'
 import DocsLayout from '../../../components/docs/DocsLayout/DocsLayout'
 
 export default function ContributingPage() {
@@ -8,7 +8,44 @@ export default function ContributingPage() {
         Contributing to MetaKB
       </Typography>
 
-      <Typography></Typography>
+      <Typography component="p" sx={{ mb: 2 }}>
+       MetaKB Jr is an open source project and contributions are encouraged.
+      </Typography>
+
+      <Typography component="p" sx={{ mb: 2 }}>
+       Ways to contribute include:
+      </Typography>
+
+<Box component="ul" sx={{ pl: 4, mt: 0 }}>
+  <li>
+    <Typography>
+      Reporting bugs
+    </Typography>
+  </li>
+
+  <li>
+    <Typography>
+      Suggesting new features
+    </Typography>
+  </li>
+
+  <li>
+    <Typography>
+      Suggesting new data sources
+    </Typography>
+  </li>
+  <li>
+    <Typography>
+      Contributing code
+    </Typography>
+  </li>
+  <li>
+    <Typography>
+      Participating in discussions on GitHub
+    </Typography>
+  </li>
+
+</Box>
     </DocsLayout>
   )
 }
