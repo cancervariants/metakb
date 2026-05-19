a43e79c735d23582f663c43df4f78b5371b70382
Matthew Cannon <mcannon068nw@gmail.com>
2026-05-19 12:57:41 -0400

    Overview page + Team page

--- client/src/pages/Docs/About/Overview.tsx
+++ client/src/pages/Docs/About/Overview.tsx
@@ -1,34 +1,51 @@
-import { Typography } from '@mui/material'
+import { Typography, Box } from '@mui/material'
 import DocsLayout from '../../../components/docs/DocsLayout/DocsLayout'
 
 export default function OverviewPage() {
   return (
     <DocsLayout>
       <Typography variant="h4" mb={2} fontWeight="bold">
-        MetaKB Jr. Overview
+        Overview
       </Typography>
 
-      <Typography>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-      </Typography>
-      <Typography>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-      </Typography>
-      <Typography>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-      </Typography>
+<Typography component="p" sx={{ mb: 2 }}>
+  MetaKB Jr. is an open, community-driven knowledgebase designed to make
+  clinically relevant somatic variant interpretation data easier to explore,
+  integrate, and reuse. MetaKB aggregates and harmonizes data from multiple
+  precision oncology knowledgebases into a singular, consistent data model
+  with a focus on pediatric data.
+</Typography>
+
+<Typography component="p" sx={{ mb: 2 }}>
+  MetaKB Jr. is intended for researchers, bioinformaticians, educators,
+  variant classification experts, and the broader precision oncology
+  community.
+</Typography>
+
+<Typography component="p" sx={{ mb: 1 }}>
+  The goal of MetaKB Jr. is to lower the barrier to working with variant
+  classification data by providing:
+</Typography>
+
+<Box component="ul" sx={{ pl: 4, mt: 0 }}>
+  <li>
+    <Typography>
+      Harmonized terminology and structured relationships
+    </Typography>
+  </li>
+
+  <li>
+    <Typography>
+      Aggregated classification knowledge
+    </Typography>
+  </li>
+
+  <li>
+    <Typography>
+      Transparent provenance and attribution
+    </Typography>
+  </li>
+</Box>
     </DocsLayout>
   )
 }
