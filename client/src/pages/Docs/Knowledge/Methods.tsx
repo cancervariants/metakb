2ca455d5761811d63a129aa38cd1a556d0dbe7d9
Matthew Cannon <mcannon068nw@gmail.com>
2026-05-20 08:25:13 -0400

    data model

--- client/src/pages/Docs/Knowledge/Methods.tsx
+++ client/src/pages/Docs/Knowledge/Methods.tsx
@@ -1,4 +1,4 @@
-import { Typography } from '@mui/material'
+import { Typography, Box } from '@mui/material'
 import DocsLayout from '../../../components/docs/DocsLayout/DocsLayout'
 
 export default function MethodsPage() {
@@ -8,7 +8,41 @@ export default function MethodsPage() {
         MetaKB Data Transformation Methods
       </Typography>
 
-      <Typography></Typography>
+      <Typography component="p" sx={{ mb: 2 }}>
+        MetaKB Jr uses a modular data ingestion and transformation pipeline to normalize heterogeneous variant classification datasets into a unified schema. Our data model emphasizes reproducibility and transparent transformation workflows.
+      </Typography>
+
+    <Typography component="p" sx={{ mb: 2}}>
+      Core processing steps include:
+    </Typography>
+
+    <Box component="ul" sx={{ pl: 4, mt: 0 }}>
+  <li>
+    <Typography>
+      Data ingest from public sources
+    </Typography>
+  </li>
+
+  <li>
+    <Typography>
+      Parsing and normalization of records
+    </Typography>
+  </li>
+
+  <li>
+    <Typography>
+      Mapping entities to standardized identifiers and ontologies
+    </Typography>
+  </li>
+  <li>
+    <Typography>
+      Aggregation and display of searchable evidence, therapies, diseases, and classifications
+    </Typography>
+  </li>
+
+</Box>
+
+
     </DocsLayout>
   )
 }
