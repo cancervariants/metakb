d65429fbcca5477f3199ab8742bc274e4555e3bf
Matthew Cannon <87494086+mcannon068nw@users.noreply.github.com>
2026-05-21 14:30:50 -0400

    feat: Fill in info pages (#837)
    Fill in content for the static information pages. Not all the way done
    yet but open to suggestions for content/changes on the pages thus far
    
    ---------
    
    Co-authored-by: Matthew Cannon <mcannon068nw@gmail.com>
    Co-authored-by: James Stevenson <james.stevenson@nationwidechildrens.org>

--- client/src/pages/Docs/Knowledge/DataModel.tsx
+++ client/src/pages/Docs/Knowledge/DataModel.tsx
@@ -1,4 +1,4 @@
-import { Typography } from '@mui/material'
+import { Typography, Box } from '@mui/material'
 import DocsLayout from '../../../components/docs/DocsLayout/DocsLayout'
 
 export default function DataModelPage() {
@@ -6,6 +6,38 @@ export default function DataModelPage() {
     <DocsLayout>
       <Typography variant="h4" mb={2} fontWeight="bold">
         MetaKB Data Model
+      </Typography>
+
+      <Typography component="p" sx={{ mb: 2 }}>
+        The MetaKB Jr data model uses a knowledge graph approach designed to represent complex
+        relationships between variant classification data. This schema supports the following data
+        types:
+      </Typography>
+
+      <Box component="ul" sx={{ pl: 4, mt: 0 }}>
+        <li>
+          <Typography>Genes</Typography>
+        </li>
+
+        <li>
+          <Typography>Variants</Typography>
+        </li>
+
+        <li>
+          <Typography>Diseases</Typography>
+        </li>
+        <li>
+          <Typography>Evidence statements</Typography>
+        </li>
+        <li>
+          <Typography>Publications</Typography>
+        </li>
+      </Box>
+
+      <Typography component="p" sx={{ mb: 2 }}>
+        Data objects in MetaKB are represented under the GA4GH Genomic Knowledge Standards,
+        including use of the Variant Annotation Specification (VA-Spec) and Categorical Variant
+        Representation Specification (CatVRS).
       </Typography>
 
       <Typography></Typography>
