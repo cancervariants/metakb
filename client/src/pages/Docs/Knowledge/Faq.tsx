f025ba9c0e6037e656d0660fadb3abfb5993ec6c
James Stevenson <james.stevenson@nationwidechildrens.org>
2026-07-10 12:47:33 -0400

    ope

--- client/src/pages/Docs/Knowledge/Faq.tsx
+++ client/src/pages/Docs/Knowledge/Faq.tsx
@@ -18,7 +18,7 @@ function FAQItem({ question, answer }: { question: string; answer: string }) {
 
 export default function FAQPage() {
   useEffect(() => {
-    document.title = 'MetaKB Jr. | FAQ'
+    document.title = 'FAQ | MetaKB Jr.'
   }, [])
 
   return (
