4aa7be25248f189ab102927f71005d7ee667be48
James Stevenson <james.stevenson@nationwidechildrens.org>
2026-09-22 16:36:55 -0400

    fix

--- server/tests/unit/transformers/test_catvars.py
+++ server/tests/unit/transformers/test_catvars.py
@@ -9,9 +9,9 @@ from ga4gh.cat_vrs.models import (
     DefiningAlleleConstraint,
 )
 from ga4gh.vrs import models
+from metakb.transformers.catvars import get_normalized_protein_consequence_name
 
 from metakb.repository.neo4j_models import CategoricalVariantNode
-from metakb.transformers.catvars import get_normalized_protein_consequence_name
 
 
 @pytest.fixture
